c4213b65e22fbdbc2594acf9fc6808edc99f97d2
Felix Klumpp <felix-k@gmx.com>
2025-06-12 19:31:04 +0200

    Main Upgrade: No reboot

--- .local/bin/nixos-main-upgrade.sh
+++ .local/bin/nixos-main-upgrade.sh
@@ -10,10 +10,7 @@ sudo -u felix nix flake update --commit-lock-file
 #sudo -u felix git stage flake.lock
 #sudo -u felix git commit  -am "gml - automatic upgrade"
 
-nixos-rebuild boot --upgrade --print-build-logs
+nixos-rebuild switch --upgrade --print-build-logs
 
 sudo -u felix git push origin main
 
-sleep 120
-
-reboot
