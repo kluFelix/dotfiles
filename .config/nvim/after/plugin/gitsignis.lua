506a7bffcc114230de52603a157af0de08127f90
Felix Klumpp <felix-k@gmx.com>
2025-10-25 22:47:11 +0200

    nvim: better gitsigns settings

---
+++ .config/nvim/after/plugin/gitsignis.lua
@@ -0,0 +1,7 @@
+require('gitsigns').setup {
+  signs_staged_enable = true,
+  signcolumn = false,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+}
